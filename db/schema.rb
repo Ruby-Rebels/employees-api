64793d448d6864d8023880486f037a75b0b8ee7c
Trevor Jones <trevordavidjones@gmail.com>
2016-09-12 20:25:37 -0400

    protect API

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160817004851) do
+ActiveRecord::Schema.define(version: 20160912232204) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20160817004851) do
     t.date     "birthdate"
     t.string   "email"
     t.string   "ssn"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "api_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
